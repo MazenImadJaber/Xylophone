0af0548c3d600149ad8afbd987937642fbb8b617
Mazen Imad Jaber <57973260+MazenImadJaber@users.noreply.github.com>
2022-06-27 18:00:35 +1000

    add timer to opacity challenge complited

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -5,21 +5,29 @@ class ViewController: UIViewController {
     
     var player: AVAudioPlayer!
 
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
         let key =  sender.titleLabel?.text;
-       playSound(key: key! )
-      //  print(key)
+        
+        sender.alpha = 0.5
+        if(playSound(key: key! )){
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                sender.alpha = 1;
+            }
+        }
+
     }
     
-    func playSound(key: String) {
+    func playSound(key: String)-> Bool{
         let url = Bundle.main.url(forResource: key, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
-        player.play()
+        return player.play()
                 
     }
+   
 }
 
